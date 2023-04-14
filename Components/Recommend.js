9ac0a21d52a98e4fdb150308dc818211619aeebb
kartik-raj7 <kartikeyrajgupta007@gmail.com>
2023-04-15 01:12:39 +0530

    new rep

--- Components/Recommend.js
+++ Components/Recommend.js
@@ -1,23 +1,24 @@
-import Bestseller from '../public/Bestseller.jpg'
-import Drinks from '../public/Drinks.jpg'
-import Food from '../public/Food.jpg'
-import Merchandise from '../public/Merchandise.jpg'
-import CoffeeatHome from '../public/Coffee At Home.jpg'
-import ReadytoEat from '../public/Ready to Eat.jpg'
-import leaf from '../public/leaf.svg' 
-import Image from 'next/image'
+import Bestseller from '../public/Bestseller.jpg';
+import Drinks from '../public/Drinks.jpg';
+import Food from '../public/Food.jpg';
+import Merchandise from '../public/Merchandise.jpg';
+import CoffeeatHome from '../public/Coffee At Home.jpg';
+import ReadytoEat from '../public/Ready to Eat.jpg';
+import Image from 'next/image';
+import styles from './Recommend.module.scss'
+import leaf from '../public/leaf.svg';
+
 const Recommend = () => {
-    return (
-        <div className='bg-[#edebe9]'>
-        <div className='justify-between py-4 flex flex-row '>
-            <h2 className=' mt-5 ml-4 text-[#1e393]'>Barista Recommends</h2>
-            <div className='flex flex-row'>
-            <h2 className='text-[#00754a] mt-5 mr-10'>View Menu</h2>
-            <Image src={leaf} width={60} height={60} alt='photo'/>
-            </div>
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Barista Recommends</h2>
+      <div className={styles.action}>
+        <h2 className={styles.menu}>View Menu</h2>
+        <div className={styles.image}>
         </div>
+      </div>
     </div>
-    )
-  }
-  
-export default Recommend
+  );
+};
+
+export default Recommend;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
-import { Inter } from '@next/font/google'
-import Navigation from '../Components/Navigation'
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+import Recommend from '@/Components/Recommend'
+import Navigation from '@/Components/Navigation'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-      {/* <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1> */}
+     <Navigation/>
+     <Recommend/>
     </>
   )
 }
