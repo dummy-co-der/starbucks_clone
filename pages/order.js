ba4c27ca2cb99d4b3378da5d6a4b9c62d5ef6872
Anuj Maheshwari <maheshwarianuj1001@gmail.com>
2023-05-13 17:44:52 +0530

    build changes

--- pages/order.js
+++ pages/order.js
@@ -2,13 +2,12 @@ import React from 'react'
 import Layout from './Layout'
 import StoreLocation from '@/Components/Order/StoreLocation'
 import OrderOptionFilter from '@/Components/Order/OrderOptionFilter'
-import BestSeller from '@/Components/Order/BestSeller'
+
 const order = () => {
   return (
     <Layout>
       <StoreLocation/>
       <OrderOptionFilter/>
-      {/* <BestSeller/> */}
     </Layout>
   )
 }
