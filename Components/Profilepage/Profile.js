a114cf58c7147f0bdca30b52b9818b8ac3bf8c26
dummy-co-der <maheshwarianuj1001@gmail.com>
2023-06-01 00:03:32 +0530

    profile

--- Components/Profilepage/Profile.js
+++ Components/Profilepage/Profile.js
@@ -1,6 +1,6 @@
 import { Button } from 'antd'
 import React from 'react'
-import logo from '../../public/profileDP.svg'
+// import logo2 from '../../public/Profile-images/profileDP.svg'
 import styles from './profilepagestyle/Profile.module.scss'
 import { RightOutlined, SettingFilled, UserOutlined } from '@ant-design/icons'
 import Link from 'next/link'
@@ -15,7 +15,7 @@ const Profile = () => {
         </div>
         <div style={{display:'flex',flexDirection:'column'}}>
         <div className={styles.imagesbox}>
-        <img src='/profileDp.svg' alt="profile"/>
+        <img src="/Profile-images/profileDP.svg" alt="profile"/>
         </div>
         <h2>Welcome to Starbucks</h2>
         <Button className={styles.buttonstyle}>Login or Signup</Button>
